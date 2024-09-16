0970334b2a2c416f7eb6eccf89325a7b92676fc2
Jon Aldekoa <jaldekoa@gmail.com>
2024-09-16 12:29:33 +0200

    Modify tests

--- bankfindapi/structure/__init__.py
+++ bankfindapi/structure/__init__.py
@@ -10,7 +10,7 @@ def get_institutions(**kwargs) -> pd.DataFrame:
 
     Keyword Args:
         filters (str): The filter for the bank search.
-        search (str): Flexible text search against institution records - currently only supporting name search. Search supports text search and fuzzy matching, as opposed to filters that are exact matches. **None by default**.
+        search (str): Flexible text search against institution records - currently only supporting name search. Search supports text search and fuzzy matching, as opposed to filters that are exact matches.
         fields (str): Comma delimited list of fields to search.
         sort_by (str): Field name by which to sort returned data.
         sort_order (str): Indicator if ascending (ASC) or descending (DESC).
--- bankfindapi/tests/test_structute.py
+++ bankfindapi/tests/test_structute.py
@@ -4,7 +4,23 @@ import pandas as pd
 
 def test_get_institutions():
     assert isinstance(get_institutions(), pd.DataFrame)
+    df = get_institutions(filters="STALP:IA AND ACTIVE:1",
+                          #search="NAME:Island",
+                          fields="ZIP,OFFDOM,CITY,COUNTY,STNAME,STALP,NAME,ACTIVE,CERT,CBSA,ASSET,NETINC,DEP,DEPDOM,"
+                                 "ROE,ROA,DATEUPDT,OFFICES",
+                          sort_by="OFFICES",
+                          sort_order="DESC",
+                          limit=10,
+                          offset=0)
+    assert isinstance(df, pd.DataFrame)
 
 
 def test_get_locations():
     assert isinstance(get_locations(), pd.DataFrame)
+    df = get_institutions(filters="STALP:IA AND ACTIVE:1",
+                          fields="NAME,UNINUM,SERVTYPE,RUNDATE,CITY,STNAME,ZIP,COUNTY",
+                          sort_by="NAME",
+                          sort_order="DESC",
+                          limit=10,
+                          offset=0)
+    assert isinstance(df, pd.DataFrame)
